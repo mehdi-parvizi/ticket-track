3985c90188cb87ca45c5f8f9a34578760aafcf6a
Mehdi <aceparvizi@gmail.com>
2025-01-08 13:39:59 +0100

    add aditional loading sstates

--- src/app/issues/[id]/loading.tsx
+++ src/app/issues/[id]/loading.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { Card } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>
-      <div>loading...</div>
+    <div className="max-w-xl">
+      <Skeleton />
+      <div className="flex space-x-3 my-2 items-center">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </div>
+      <Card className="prose mt-4">
+        <Skeleton count={3} />
+      </Card>
     </div>
   );
 };
--- src/app/issues/new/loading.tsx
+++ src/app/issues/new/loading.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingNewIssuePage = () => {
   return (
-    <div>
-      <div>loading...</div>
+    <div className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
     </div>
   );
 };

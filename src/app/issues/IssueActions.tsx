95ca00b6f56593afcd3ddb8e29ff15388faf82a7
Mehdi <aceparvizi@gmail.com>
2025-01-09 13:24:09 +0100

    build the filter component

--- src/app/issues/IssueActions.tsx
+++ src/app/issues/IssueActions.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import { routes } from "../routes/routes";
+import StatusFilter from "./StatusFilter";
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
+    <div className="mb-5 flex justify-end gap-5">
+      <StatusFilter />
       <Button>
         <Link href={routes.newIssues.href}>{routes.newIssues.label}</Link>
       </Button>

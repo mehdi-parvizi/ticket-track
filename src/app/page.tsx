97a1085daae44f46737918e8509c68c57e89eee5
Mehdi <aceparvizi@gmail.com>
2025-01-10 10:24:05 +0100

    build pagination layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+    </div>
+  );
 }

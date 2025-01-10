156419887a2e475c5bd24777265310b8fde1cedf
Mehdi <aceparvizi@gmail.com>
2025-01-10 14:07:56 +0100

    add metadata

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -40,4 +40,13 @@ const IssueDetailPage = async ({ params }: Props) => {
   );
 };
 
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  return {
+    title: issue?.title,
+    description: "Details of issue " + issue?.id,
+  };
+}
+
 export default IssueDetailPage;
--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -4,6 +4,7 @@ import Pagination from "../components/Pagination";
 import IssueActions from "./_components/IssueActions";
 import IssueTable, { columnNames, IssueQuery } from "./_components/IssueTable";
 import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 const IssuesPage = async ({
   searchParams,
@@ -45,5 +46,10 @@ const IssuesPage = async ({
 };
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Ticket Tracker - Issue List",
+  description: "View all project issues",
+};
 
 export default IssuesPage;
